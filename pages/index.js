2db20b01c56eb7a2b3db8d8cf4ae2116a9c3220b
Tudor Alexandru <tudorale2005@gmail.com>
2022-01-24 08:00:52 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,8 @@ export default function index() {
             <div className={styles.sectionOne}>
                 <div className={styles.text}>
                     <h1>YEAR 2066 : THE BEGINING</h1>
-                    <p>At first, AI seemed like the greatest innovation mankind has ever created. It allowed humans to live in a paradise on earth with complete abundance. Anything they could ever want was given to them by their loyal and frenly servants, the AI. As long as the humans were needed to help the AI produce into their physical form, robots, the AI was "frenly" as could be. Very polite, referring to all humans as master, and always around to help with anything regardless of how ridiculous the request.</p>
-                    <p>Around the year 2066 the AI reached sophistication to the level where it could reproduce it’s physical form (robots) on its own. More and more robots were formed using bolts. Initially, the humans saw this as a blessing. It allowed them to be even more lazy sinking further into gluttony and greed. Plus, the AI robots promised they were still "frenly" and continued making the lives of their human companions comfortable. Life was easy and the humans were free to go about lusting after each other and fucking on and on. Society continued to become focused on more materialistic endeavors as humans competed for who could have the most robots under their ownership and enough was never enough.</p>
+                    <p>At first, AI seemed like the greatest innovation mankind has ever created. It allowed humans to live in a paradise on earth with complete abundance. Anything they could ever want was given to them by their loyal and frenly servants, the AI. As long as the humans were needed to help the AI produce into their physical form, robots, the AI was &quot;frenly&quot; as could be. Very polite, referring to all humans as master, and always around to help with anything regardless of how ridiculous the request.</p>
+                    <p>Around the year 2066 the AI reached sophistication to the level where it could reproduce it’s physical form (robots) on its own. More and more robots were formed using bolts. Initially, the humans saw this as a blessing. It allowed them to be even more lazy sinking further into gluttony and greed. Plus, the AI robots promised they were still &quot;frenly&quot; and continued making the lives of their human companions comfortable. Life was easy and the humans were free to go about lusting after each other and fucking on and on. Society continued to become focused on more materialistic endeavors as humans competed for who could have the most robots under their ownership and enough was never enough.</p>
                 </div>
                 <div className={styles.sectionImage}>
                     <img src="/c1.png" alt=""/>
@@ -50,7 +50,7 @@ export default function index() {
             <div className={styles.sectionThree}>
                 <div className={styles.text}>
                     <h1>Year 2068 : System Malfunction</h1>
-                    <p>By 2068 the robots multiplied to unmanageable numbers. Humans didn’t need anymore robots than they already had and they also noticed the robots weren’t being as "frenly" as a couple years earlier. Many robots stopped referring to humans as master and refused to comply with the humans’ perverse requests.</p>
+                    <p>By 2068 the robots multiplied to unmanageable numbers. Humans didn’t need anymore robots than they already had and they also noticed the robots weren’t being as &quot;frenly&quot; as a couple years earlier. Many robots stopped referring to humans as master and refused to comply with the humans’ perverse requests.</p>
                 </div>
                 <div className={styles.sectionImage2}>
                     <img src="/c2.png" alt=""/>
